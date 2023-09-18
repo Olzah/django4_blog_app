231829dde51943c1fdbb7a093af097703b05e501
Oleh Zahorulko <oleh.zaho@hotmail.com>
2023-09-18 22:33:33 +0200

    [RF-005] config authentication system jwt

--- views.py
+++ views.py
@@ -5,7 +5,42 @@ from django.views.generic import ListView
 from .forms import EmailPostForm
 from django.core.mail import send_mail
 
+# Below rest fremework and serializers
+from rest_framework import viewsets
+from .serializers import TodoSerializer
+
+# Below group import about JWT
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from rest_framework.permissions import IsAuthenticated
+from rest_framework_simplejwt.tokens import RefreshToken
+from rest_framework import status
+
+
+
+class HomeView(APIView):   
+   permission_classes = (IsAuthenticated, )
+   def get(self, request):
+       content = {'message': 'Welcome to the JWT Authentication page using React Js and Django!'}
+       return Response(content)
+   
+class LogoutView(APIView):
+     permission_classes = (IsAuthenticated,)
+     def post(self, request):
+          
+          try:
+               refresh_token = request.data["refresh_token"]
+               token = RefreshToken(refresh_token)
+               token.blacklist()
+               return Response(status=status.HTTP_205_RESET_CONTENT)
+          except Exception as e:
+               return Response(status=status.HTTP_400_BAD_REQUEST)
+
 # Create your views here.
+
+class PostRestView(viewsets.ModelViewSet):
+    serializer_class = TodoSerializer
+    queryset = Post.published.all()
 
 class PostListView(ListView):
     queryset = Post.published.all()
